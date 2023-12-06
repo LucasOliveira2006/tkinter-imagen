fb87b0dd0a46f7493be9ab9621d1528353c0dca5
Lucas De Oliveira <luc.oliveira@gmail.com>
2023-12-06 20:44:27 -0300

    Adicionado Barra do Menu

--- tkinter_image.py
+++ tkinter_image.py
@@ -1,8 +1,44 @@
 from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
 from PIL import ImageTk, Image, ImageOps
 import os 
 
+def open_file():
+    folder_path= filedialog.askdirectory()
+    if folder_path:
+
+        messagebox.showinfo(
+            title='Abrindo diretório...',
+            message=f'O arquivo selecionado foi: {folder_path}'
+)
+    else:
+        messagebox.showerror(
+            title='Erro ao abrir diretório',
+            message='Nenhum diretório foi selecionado'
+        )
+
+
 root = Tk()
+root.title("Imagens engraçadas e aleatorias :)")
+root.iconbitmap("scaramouch.ico")
+
+#Cria uma barra de menu
+menubar = Menu(root)
+
+filemenu = Menu(menubar, tearoff=0)
+
+filemenu.add_command(   
+    label="Open",
+    command=open_file
+)
+filemenu.add_command(label='Save')
+filemenu.add_command(label='Exit')
+
+menubar.add_cascade(label="File", menu=filemenu)
+
+root.config(menu=menubar)
+
 #Lista os arquivos da pasta imagens
 arquivos = os.listdir("imagens")
 
@@ -14,13 +50,17 @@ imagen_atual = 0
 
 #percorre a lista de arquivos
 for arquivo in arquivos:
-    #Abre a imagen
-    img= Image.open("imagens/"+ arquivo)
-    #Redimensiona a imagen
-    img= ImageOps.contain(img, (600,600))
-    
-    #adiciona a imagen na lista
-    imagens.append(ImageTk.PhotoImage(img))
+    try:
+        #Abre a imagen
+        img= Image.open("imagens/"+ arquivo)
+    except Exception as e:
+        print(e)
+        continue
+    else:
+        #Redimensiona a imagen
+        img= ImageOps.contain(img, (400,400))
+        #adiciona a imagen na list
+        imagens.append(ImageTk.PhotoImage(img))
 
 #Exibe os arquivos em um Label
 #arquivos_label = Label(root, text=arquivos)
@@ -29,7 +69,6 @@ for arquivo in arquivos:
 #img_Tk = ImageTk.PhotoImage(img)
 
 img_label = Label(root, image=imagens[imagen_atual])
-
 img_label.grid(column=2, row=0,columnspan=3)
 
 def prev_image():
@@ -60,17 +99,21 @@ def next_image():
     img_label.grid_forget()
 
     img_label = Label(root, image=imagens[imagen_atual])
-    img_label.grid(column=2, row=0, columnspan=3)
+    img_label.grid(column=2, row=0, columnspan=5)
 
 
 
-Prev = Button(root, text="Prev", command=prev_image)
-Prev.grid(column=1, row=1,columnspan=3)
+Prev = Button(root, text="Prev", command=prev_image, bg ="#cb4a34", fg="#f6f0e9")
+Prev.grid(column=1, row=1, columnspan=3,)
 
-Sair = Button(root, text="Sair", command=root.quit)
-Sair.grid(column=2, row=1,columnspan=3)
+Sair = Button(root, text="Sair", command=root.quit, bg ="#1c1f21", fg="#f6f0e9")
+Sair.grid(column=2, row=1, columnspan=3)
 
-Next = Button(root, text="Next", command=next_image)
-Next.grid(column=3, row=1,columnspan=3)
+Next = Button(root, text="Next", command=next_image, bg ="#cb4a34", fg="#f6f0e9")
+Next.grid(column=3, row=1, columnspan=3,)
+
+root.bind('<Right>', lambda event:next_image())
+root.bind('<Left>', lambda event:prev_image())
+root.bind('<Escape>', lambda event:root.quit())
 
 root.mainloop() 
